02344bedb02d4fe8e69d9dfc4ca9fb099dd7e9cd
pravali20 <166683722+pravali20@users.noreply.github.com>
2024-05-15 11:07:51 +0530

    expense.py

--- expense.py
+++ expense.py
@@ -3,5 +3,7 @@ class Expense:
         self.name = name
         self.category = category
         self.amount = amount
+    def __repr__(self):
+        return f"Expense(name={self.name}, amount={self.amount}, category={self.category})"
 
     
